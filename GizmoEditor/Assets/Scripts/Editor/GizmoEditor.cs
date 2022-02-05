f0e02fcb026e6ea1b7c3c8fdafb486aa0f403be6
Benjamin-deLaverny <benjamin.de-laverny@etu.univ-cotedazur.fr>
2022-02-05 12:27:26 +0100

    Creating project and Adding EditorWindow

---
+++ GizmoEditor/Assets/Scripts/Editor/GizmoEditor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+using technical.test.editor;
+
+public class GizmoEditor : EditorWindow
+{
+    private SceneGizmoAsset gizmoAsset = null;
+
+    [MenuItem("Window/Custom/Show Gizmos in Scene")]
+    public static GizmoEditor openWindow(){
+        return EditorWindow.GetWindow<GizmoEditor>("Gizmo Editor");
+    }    
+
+    [UnityEditor.Callbacks.OnOpenAsset(1)]
+    public static bool openGizmoAsset(int id, int line){
+        SceneGizmoAsset asset = EditorUtility.InstanceIDToObject(id) as SceneGizmoAsset;
+        if(asset != null){
+            GizmoEditor gizmoEditor = openWindow();
+            gizmoEditor.gizmoAsset = asset;
+            return true;
+        }
+        return false;
+    }
+
+    private void OnGUI(){
+        if(gizmoAsset != null){
+            int i = 0;
+            foreach(var gizmo in gizmoAsset._gizmos){
+                GUI.Label(new Rect(10,10+i,100,20),gizmo.Name);
+                i+=20;
+            }
+        }
+    }
+}
